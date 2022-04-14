a20e967b7b6366d08630436656f9f9bb0604ceb2
Sarah Badibanga <sarahbadibanga@Sarahs-MacBook-Pro.local>
2022-04-14 21:29:55 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header">
+      <div class="container">
+        <nav>
+          <a href="/"class="Rlogo"><h4>At Home</h4></a>
+          <ul class="nav-links">
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#resturant">Resturant</a></li>
+            <li><a href="/#/signin">Sign-In</a></li>
+            <li><a href="/#/cart">Cart</a></li>
+          </ul>
+          <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </nav>
+      </div>
+    </header>
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
